246d7d2a8755b31f35b52c14b21a664995e4b545
jia-er <15222617266@163.com>
2019-12-08 23:14:40 +0800

    join

---
+++ app/src/main/java/com/example/coolweather/db/Province.java
@@ -0,0 +1,40 @@
+package com.example.coolweather.db;
+
+/**
+ * Created by Oh  Yeah! on 2019/12/8.
+ */
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+
+    private int id;//每个实体类都有的字段
+
+    private String provinceName;//记录省份名字
+
+    private int provinceCode;//记录省的代号
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
